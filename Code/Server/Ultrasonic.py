a7b80ec65e4953622f9bac231643d592a7fad81f
DongtaiS <dongtaishi.w@gmail.com>
2022-11-15 15:29:49 -0500

    Updating prototype

--- Code/Server/Ultrasonic.py
+++ Code/Server/Ultrasonic.py
@@ -101,4 +101,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
         PWM.setMotorModel(0,0,0,0)
         ultrasonic.pwm_S.setServoPwm('0',90)
-
