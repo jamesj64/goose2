d9d565762ec114b8213e5df90b5f8d0a83243f82
jamesj64 <102470405+jamesj64@users.noreply.github.com>
2022-11-23 23:12:14 -0500

    Update main.js

--- Code/Server/templates/static/js/main.js
+++ Code/Server/templates/static/js/main.js
@@ -5,7 +5,11 @@ function toggleBuzzer() {
     $.post("/", {
         buzzer: 1,
         moveForward: 0,
-        stopMovement: 0
+        stopMovement: 0,
+        moveBackward: 0,
+        moveRight: 0,
+        moveLeft: 0,
+        getDistance: 0
     }, function(err, req, resp) {
         if (req == "success") {
             console.log("what the?! it works!");
@@ -19,7 +23,11 @@ function moveForward() {
     $.post("/", {
         buzzer: 0,
         moveForward: 1,
-        stopMovement: 0
+        stopMovement: 0,
+        moveBackward: 0,
+        moveRight: 0,
+        moveLeft: 0,
+        getDistance: 0
     }, function(err, req, resp) {
         if (req == "success") {
             console.log("what the?! it works!");
@@ -33,7 +41,83 @@ function stopMovement() {
     $.post("/", {
         buzzer: 0,
         moveForward: 0,
-        stopMovement: 1
+        stopMovement: 1,
+        moveBackward: 0,
+        moveRight: 0,
+        moveLeft: 0,
+        getDistance: 0
+    }, function(err, req, resp) {
+        if (req == "success") {
+            console.log("what the?! it works!");
+        } else {
+            console.log("error");
+        }
+    });
+}
+
+function moveRight() {
+    $.post("/", {
+        buzzer: 0,
+        moveForward: 0,
+        stopMovement: 0,
+        moveBackward: 0,
+        moveRight: 1,
+        moveLeft: 0,
+        getDistance: 0
+    }, function(err, req, resp) {
+        if (req == "success") {
+            console.log("what the?! it works!");
+        } else {
+            console.log("error");
+        }
+    });
+}
+
+function moveBackward() {
+    $.post("/", {
+        buzzer: 0,
+        moveForward: 0,
+        stopMovement: 0,
+        moveBackward: 1,
+        moveRight: 0,
+        moveLeft: 0,
+        getDistance: 0
+    }, function(err, req, resp) {
+        if (req == "success") {
+            console.log("what the?! it works!");
+        } else {
+            console.log("error");
+        }
+    });
+}
+
+function moveLeft() {
+    $.post("/", {
+        buzzer: 0,
+        moveForward: 0,
+        stopMovement: 0,
+        moveBackward: 0,
+        moveRight: 0,
+        moveLeft: 1,
+        getDistance: 0
+    }, function(err, req, resp) {
+        if (req == "success") {
+            console.log("what the?! it works!");
+        } else {
+            console.log("error");
+        }
+    });
+}
+
+function getDistance() {
+    $.post("/", {
+        buzzer: 0,
+        moveForward: 0,
+        stopMovement: 0,
+        moveBackward: 0,
+        moveRight: 0,
+        moveLeft: 0,
+        getDistance: 1
     }, function(err, req, resp) {
         if (req == "success") {
             console.log("what the?! it works!");
