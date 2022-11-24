123cafc0f3af662ef22e1a7ee56bd293024bba0f
Catherine Kim <80084168+catherinek07@users.noreply.github.com>
2022-11-23 22:11:03 -0500

    Update myServer.py

--- Code/Server/myServer.py
+++ Code/Server/myServer.py
@@ -4,10 +4,12 @@
 from flask import Flask
 from flask import render_template, make_response
 from flask import redirect, request, jsonify, url_for
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import os
-
+GPIO.setwarnings(False) #
 Buzzer_Pin = 17
+GPIO.setmode(GPIO.BCM)#
+GPIO.setup(Buzzer_Pin,GPIO.OUT)#
 
 app = Flask(__name__)
 app._static_folder = os.path.abspath("templates/static/")
@@ -15,12 +17,15 @@ app._static_folder = os.path.abspath("templates/static/")
 def hello_world():
     if request.method == 'POST':
         content = request.form
-        print(content['buzzer'])
-        bState = True if content['buzzer'] else False
+        bState = True if content['buzzer'] == "0" else False
+        if bState:#
+            GPIO.output(Buzzer_Pin,GPIO.HIGH)#
+        else:#
+            GPIO.output(Buzzer_Pin,GPIO.LOW)#
         #GPIO.output(Buzzer_Pin, bState)
         return ""
     else:
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='192.168.137.106', port=5050, debug=True, use_reloader=False, threaded=False)
